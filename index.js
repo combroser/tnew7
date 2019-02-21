7a19d9685faa0d299968bc5adba2c22936c557a2
combroser <42646448+combroser@users.noreply.github.com>
2019-02-21 09:55:36 -0500

    Revert "Merge branch 'master' of https://github.com/combroser/tnew7"
    This reverts commit 328e199b65a0bb2df3902f64a616b3a3670e9e0a, reversing
    changes made to 8efb470d047f5e4825af61f41ab9a55dfbe790eb.

--- index.js
+++ index.js
@@ -72,12 +72,6 @@ $(window).on("load resize", function(){
 
     })
   });
+
+
 })
-
-
-
- // Move PDP logo block
-$( init );
-function init() {
-  $('.tn-event-detail__additional-events-container').append( $('.logo-block') );
-}
